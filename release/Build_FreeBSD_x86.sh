66805b90e17b988e95e58ad3d4e998fe0089ccf1
Jacky Lee <JackyLeeHongJian@Gmail.com>
2012-06-29 21:15:39 +0800

    Recompiled idock 1.5 for FreeBSD x86 with boost 1.50.0

--- release/Build_FreeBSD_x86.sh
+++ release/Build_FreeBSD_x86.sh
@@ -1,11 +1,11 @@
 rm -f ../bin/FreeBSD/x86/idock ../obj/FreeBSD/x86/*.o
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/scoring_function.o ../src/scoring_function.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/box.o ../src/box.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/quaternion.o ../src/quaternion.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/thread_pool.o ../src/thread_pool.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/receptor.o ../src/receptor.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/ligand.o ../src/ligand.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/grid_map_task.o ../src/grid_map_task.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/monte_carlo_task.o ../src/monte_carlo_task.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/main.o ../src/main.cpp -I/home/hjli/boost_1_49_0 -c
-clang++ -m32 -static -O3 -DNDEBUG -o ../bin/FreeBSD/x86/idock ../obj/FreeBSD/x86/scoring_function.o ../obj/FreeBSD/x86/box.o ../obj/FreeBSD/x86/quaternion.o ../obj/FreeBSD/x86/thread_pool.o ../obj/FreeBSD/x86/receptor.o ../obj/FreeBSD/x86/ligand.o ../obj/FreeBSD/x86/grid_map_task.o ../obj/FreeBSD/x86/monte_carlo_task.o ../obj/FreeBSD/x86/main.o -L/home/hjli/boost_1_49_0/lib/x86 -pthread -lboost_system -lboost_thread -lboost_filesystem -lboost_program_options -lboost_iostreams -lz -lbz2
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/scoring_function.o ../src/scoring_function.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/box.o ../src/box.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/quaternion.o ../src/quaternion.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/thread_pool.o ../src/thread_pool.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/receptor.o ../src/receptor.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/ligand.o ../src/ligand.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/grid_map_task.o ../src/grid_map_task.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/monte_carlo_task.o ../src/monte_carlo_task.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -std=gnu++11 -o ../obj/FreeBSD/x86/main.o ../src/main.cpp -I/home/hjli/boost_1_50_0 -c
+clang++ -m32 -static -O3 -DNDEBUG -o ../bin/FreeBSD/x86/idock ../obj/FreeBSD/x86/scoring_function.o ../obj/FreeBSD/x86/box.o ../obj/FreeBSD/x86/quaternion.o ../obj/FreeBSD/x86/thread_pool.o ../obj/FreeBSD/x86/receptor.o ../obj/FreeBSD/x86/ligand.o ../obj/FreeBSD/x86/grid_map_task.o ../obj/FreeBSD/x86/monte_carlo_task.o ../obj/FreeBSD/x86/main.o -L/home/hjli/boost_1_50_0/lib/x86 -pthread -lboost_system -lboost_thread -lboost_filesystem -lboost_program_options -lboost_iostreams -lz -lbz2
